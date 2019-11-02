efbc9824db06216b31f9d99fbfeb698483b3acf0
Ben Grabowski <gbowski30@gmail.com>
2019-11-01 20:05:18 -0600

    Updates

---
+++ precache-manifest.d25eddf45af114d3494e537af8017b60.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18e55bb1719baa8c795bce2d69894ba4",
+    "url": "/favoreat/index.html"
+  },
+  {
+    "revision": "07fce02eed673691d5de",
+    "url": "/favoreat/static/css/main.cc522201.chunk.css"
+  },
+  {
+    "revision": "cda54c6a6ad134a9a0a2",
+    "url": "/favoreat/static/js/2.420f3ae6.chunk.js"
+  },
+  {
+    "revision": "07fce02eed673691d5de",
+    "url": "/favoreat/static/js/main.08388ddd.chunk.js"
+  },
+  {
+    "revision": "082bd8a71856f56da51c",
+    "url": "/favoreat/static/js/runtime-main.7730ec2f.js"
+  },
+  {
+    "revision": "ae1a59774186db656f7de65e9be0001d",
+    "url": "/favoreat/static/media/favoreat-background-4.ae1a5977.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1b00c8556d7607d2a108113f026c59a9.js"
+  "/favoreat/precache-manifest.d25eddf45af114d3494e537af8017b60.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/favoreat/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
